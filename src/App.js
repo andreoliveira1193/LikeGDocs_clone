c55c66d52a2afa10526f813360cf80d17e469752
Andre <a.hardware@hotmail.com>
2022-12-08 14:25:51 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Docs from './components/docs';
+import {app, database} from './firebaseConfig';
+import {Routes, Route} from 'react-router-dom';
+import EditDocs from './components/EditDocs';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Docs database={database} />} />
+      <Route path='/editDocs/:id' element={<EditDocs database= {database}/>} />
+    </Routes>
+  );
+}
+
+export default App;
